d8295617ee409941d96977c56027a5123193375b
littlewoko <watkinsbradley01@gmail.com>
2024-08-19 22:05:12 +0100

    init

---
+++ wc-tool/wc-tool/wc-tool/wc-tool/Program.cs
@@ -0,0 +1,17 @@
+﻿if(args.Length == 0)
+{
+    Console.WriteLine("Welcome to this wc tool! Please provide arguments\n-c <file path>: prints the number of bytes in file");
+    return;
+}
+
+try
+{
+    if (args[0].Equals("-c"))
+    {
+        var fileInfo = new FileInfo(args[1]);
+        Console.WriteLine($"Bytes in file: {fileInfo.Length}");
+    }
+} catch(Exception ex)
+{
+    Console.WriteLine("Something went wrong");
+}
